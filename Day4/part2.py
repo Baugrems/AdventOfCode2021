158422b66a4a6ff18289d2d62b105f3ef8c41f3c
Shane <scopenhagen@mines.edu>
2021-12-04 01:22:48 -0700

    finish day 4

---
+++ Day4/part2.py
@@ -0,0 +1,122 @@
+class Board: # object to hold a board matrix
+    def __init__(self):
+        self.matrix = []
+        self.done = False
+
+    def printBoard(self):
+        msg = ""
+        for r in self.matrix:
+            msg += "\n"
+            for sq in r:
+                if not sq.marked:
+                    msg += "[" + sq.value + "] "
+                else:
+                    msg += "[X] "
+        print(msg)
+
+
+    def didIWin(self, value): # this function will check rows and columns to mark a value then look for a bingo. Returns boolean.
+        newMark = False
+        for r in self.matrix:
+            for s in r:
+                if value == s.value:
+                    s.marked = True
+                    newMark = True
+                    break
+        if not newMark:
+            return False
+        for r in self.matrix:
+            bingo = True
+            for sq in r:
+                if not sq.marked:
+                    bingo = False
+                    break
+            if bingo:
+                return True
+        for x in range(5):
+            bingo = True
+            for y in range(5):
+                if not self.matrix[y][x].marked:
+                    bingo = False
+                    break
+            if bingo:
+                return True
+        return False
+
+
+        
+
+class Square: # each square on a board is an object to hold its status and value
+    marked = False
+    value = 0
+    def __init__(self, v):
+        self.value = v
+
+def runGame(boards, nums):
+    rem = []
+    for x in nums: # iterate over the random numbers until someone wins
+        print("Drawn Number: " + x)
+        if len(rem) > 0:
+            for r in rem:
+                boards.remove(r)
+        rem = []
+        for b in boards:
+            b.printBoard()
+            print("\n")
+            status = b.didIWin(x)
+            if status and len(boards) > 1:
+                rem.append(b)
+            elif status:
+                return b, x
+
+# Grab first line, our random bingo draws
+f = open('input')
+nums = f.readline()
+nums = nums.replace("\n", "")
+nums = nums.split(',')
+print(nums)
+# All set. Now to make boards
+
+#Make board object and all boards array
+bingo = Board()
+boards = []
+try:
+    while True:
+        line = f.readline()
+        if not line:
+            boards.append(bingo) # push completed board to array of boards
+            break
+        if line == "\n": # skip blank lines, and clear our board object to start new one
+            # print("Blank")
+            boards.append(bingo) # push completed board to array of boards
+            del bingo
+            bingo = Board()
+        else:
+            line = line.strip()
+            line = line.split()
+            row = []
+            for x in line:
+                sq = Square(x)
+                row.append(sq)
+            bingo.matrix.append(row)
+except:
+    pass
+f.close()
+boards.pop(0)
+# for b in boards:
+#     b.printBoard()
+#     print("\n")
+
+
+winner, num = runGame(boards, nums) # find winning board
+
+sum = 0 # grab sum of unmarked spots
+for r in winner.matrix:
+    for sq in r:
+        if not sq.marked:
+            sum += int(sq.value)
+print(sum * int(num))
+
+
+
+
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Each Day is saved as its own folder. Might become a mix of languages but I'm sta
 - Day 1: Python for both parts. Finished on Day 1 (December 1st).
 - Day 2: Python for both parts. Finished on Day 2 (December 2nd).
 - Day 3: Python for both parts. Finished on Day 3 (December 3rd). - This one was kind of ugly... sorry to anyone reading through it. I should have separated a few things out info functions but did not...
-- Day 4: Coming in 21 hours.
+- Day 4: Python for both parts. Finish on Day 4 (December 4th) - Starting to get heavier. Might leave Python soon. Made an object for each bingo board and each square. Gave them status variables and stored a matrix of squares on each board. For part 2 I just copied part 1 but instead of returning a winning board first, I remove boards form the array until only one was left
